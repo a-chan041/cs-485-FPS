b65468cfd2cdf63e8171f83970b8e0648e994631
a-chan041 <amanda.chan23@yahoo.com>
2019-02-12 09:12:53 -0800

    First commit

---
+++ Assets/CollideScript.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CollideScript : MonoBehaviour
+{
+    public Text countText;
+    public Text WinText;
+    private int count;
+   
+
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Pick Up"))
+        {
+            other.gameObject.SetActive(false);
+            count = count + 1;
+            SetCountText();
+
+        }
+    }
+
+
+    void SetCountText()  
+    {
+        countText.text = "Count:" + count.ToString();
+
+        if (count >= 3)
+        {
+            WinText.text = "You Win!";
+        }
+
+    }
+
+
+
+
+}
---
+++ Assets/Scripts/PlayerPOV.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerPOV : MonoBehaviour
+{
+
+    public Transform playerBody;
+    public float mouseSensitivity;
+
+    float xAxisClamp = 0.0f;
+
+    void Awake() // as soon as game starts
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    void Update()
+    {
+        RotateCamera();
+    }
+
+    void RotateCamera()
+    {
+        float MouseX = Input.GetAxis("Mouse X"); //gains access for mouse x
+        float MouseY = Input.GetAxis("Mouse Y"); // gains access for mouse y
+
+        float rotAmountX = MouseX * mouseSensitivity;
+        float rotAmountY = MouseY * mouseSensitivity;
+
+        xAxisClamp -= rotAmountY;
+
+        Vector3 targetRotCam = transform.rotation.eulerAngles; //rotation of camera angle of object
+        Vector3 targetRotBody = playerBody.rotation.eulerAngles; //rotation of camera of player body
+
+        targetRotCam.x -= rotAmountY;
+        targetRotCam.z = 0;
+        targetRotBody.y += rotAmountX;
+
+        if (xAxisClamp > 90)
+        {
+            xAxisClamp = 90;
+            targetRotCam.x = 90;
+        }
+        else if (xAxisClamp < -90)
+        {
+            xAxisClamp = -90;
+            targetRotCam.x = 270;
+        }
+
+
+        transform.rotation = Quaternion.Euler(targetRotCam);
+        playerBody.rotation = Quaternion.Euler(targetRotBody);
+    }
+
+}
